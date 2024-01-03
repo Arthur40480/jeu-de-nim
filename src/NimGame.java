9a6b2692cef1e0b053c4ce39ab17311588609143
Arthur Gibert <arthurgibert@free.fr>
2024-01-03 11:31:03 +0100

    Correction de nommage pour JeuDeNim en NimGame

--- src/NimGame.java
+++ src/NimGame.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class JeuDeNim {
+public class NimGame {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
